d5bb3a3b0120b87ab5e95160d4d343f4079b1305
kovalenkoko <kiril.kovalenko.79@outlook.com>
2022-07-26 15:37:50 +0300

    Added crud operations for video_item

--- src/controllers/videoController.ts
+++ src/controllers/videoController.ts
@@ -3,6 +3,7 @@ const uuid = require("uuid")
 const path = require("path")
 const {VideoItem} = require("../models/models")
 const ApiError = require("../error/ApiError")
+const fs = require("fs")
 
 class VideoController {
     async create(req, res, next) {
@@ -18,11 +19,54 @@ class VideoController {
             next(ApiError.badRequest(e.message))
         }
     }
-    async getAll(req, res) {
-
+    async getAll(req, res, next) {
+      try {
+          const videos = await VideoItem.findAll()
+          return res.json(videos)
+      }catch (e:any){
+          next(ApiError.badRequest(e.message))
+      }
     }
-    async getOne(req, res) {
+    async getOne(req, res, next) {
+        try {
+            const {id} = req.params
+            const video = await VideoItem.findOne({where:{id}})
+            return res.json(video)
+        }catch (e:any){
+            next(ApiError.badRequest(e.message))
+        }
+    }
+    async delete(req, res, next){
+        try {
+            const {id} = req.params
+            const videoItem = await VideoItem.findOne({where:{id}})
+            await VideoItem.destroy({where:{id}})
 
+            fs.unlink(path.resolve(__dirname, "..", "..", "static", videoItem.video), (e)=>{
+                if(e){
+                    return next(ApiError.internal(e.message))
+                }
+            })
+
+            return res.json({message: `VideoItem with id:${id} was successfully deleted`})
+        }catch (e:any){
+            next(ApiError.badRequest(e.message))
+        }
+    }
+    async update(req, res, next){
+        try {
+            const {id} = req.params
+            const {title, description} = req.body
+            const updatedObject = {
+                title: title,
+                description: description
+            }
+            await VideoItem.update(updatedObject, {where:{id}}
+            )
+            return res.json({message: `VideoItem with id:${id} was successfully updated`})
+        }catch (e:any){
+            next(ApiError.badRequest(e.message))
+        }
     }
 }
 module.exports = new VideoController()
--- src/routes/videoRouter.ts
+++ src/routes/videoRouter.ts
@@ -4,8 +4,9 @@ const videoController = require("../controllers/videoController")
 const router = new Router()
 
 router.post("/", videoController.create)
-router.get("/:id")
-router.delete("/:id")
-router.put("/:id")
+router.get("/", videoController.getAll)
+router.get("/:id", videoController.getOne)
+router.delete("/:id", videoController.delete)
+router.put("/:id", videoController.update)
 
 module.exports = router
